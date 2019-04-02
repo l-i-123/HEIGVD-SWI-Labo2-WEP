c762b5a8c0b3ce7f523f230656bbfe944aa12c79
Elie <elie.kin63@gmail.com>
2019-04-02 19:36:57 +0200

    non fonctionnel

--- files/manual-encryption.py
+++ files/manual-encryption.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-""" Manually decrypt a wep message given the WEP key"""
+""" Manually decrypt a wep message given the WEP key """
 
 __author__      = "Abraham Rubinstein"
 __copyright__   = "Copyright 2017, HEIG-VD"
@@ -16,18 +16,16 @@ import rc4
 
 #Cle wep AA:AA:AA:AA:AA
 key='\xaa\xaa\xaa\xaa\xaa'
-message = "super message encrypte"
+message = "surprise !!"
 
 #lecture de message chiffré - rdpcap retourne toujours un array, même si la capture contient un seul paquet
-arp = rdpcap('arp.cap')[0] 
-arp.show()
+arp = rdpcap('arp.cap')[0]
 
 # rc4 seed est composé de IV+clé
-seed = arp.iv+key 
-
+seed = arp.iv+key
 # Calcul du nouvel ICV en effectuant un CRC du message
-arp.icv = binascii.crc32(message)
-icv_clear='{:x}'.format(arp.icv).decode("hex")
+icv = binascii.crc32(message)
+icv_clear='{:x}'.format(icv).decode("hex")
 
 #texte en clair (message + ICV)
 message_clear=message + icv_clear
@@ -38,14 +36,16 @@ cryptedText = rc4.rc4crypt(message_clear, seed)
 #récupération del'ICV crypté
 icv_crypted=cryptedText[-4:]
 (icv_numerique,)=struct.unpack('!L', icv_crypted)
-if False:
-    #calcul du frame body en faisant keystream xor (data + ICV)
-    text_crypte=cryptedText[:-4] 
 
-    #remplacement du wepData par le message crypté
-    arp.wepdata = text_crypte
+#calcul du frame body en faisant keystream xor (data + ICV)
+text_crypte=cryptedText[:-4] 
+
+#remplacement du wepData par le message crypté
+arp.wepdata = text_crypte
 
 #remplacement de l'icv par l'icv crypté
-    arp.icv = icv_numerique
+arp.icv = icv_numerique
+
+arp.show()
 
 wrpcap("arp1.cap", arp)
