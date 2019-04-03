9173f1a3cc838ed939a102f3076c3664d08f55c9
root <root@localhost.localdomain>
2019-04-03 18:36:18 +0200

    working with 36 bytes

--- files/manual-encryption.py
+++ files/manual-encryption.py
@@ -13,23 +13,49 @@ __status__ 		= "Prototype"
 from scapy.all import *
 import binascii
 import rc4
+import os
+
+'''
+def bitstring_to_bytes(s):
+    v = int(s, 2)
+    b = bytearray()
+    while v:
+        b.append(v & 0xff)
+        v >>= 8
+    return bytes(b[::-1])
+'''
 
 #Cle wep AA:AA:AA:AA:AA
-key='\xaa\xaa\xaa\xaa\xaa'
-message = "surprise !!"
+key='\xaa\xaa\xaa\xaa\xab'
+message = "surprisemotherfuckersurprisemotherfu"
+#messageBin = binascii.a2b_base64(message)
 
 #lecture de message chiffré - rdpcap retourne toujours un array, même si la capture contient un seul paquet
 arp = rdpcap('arp.cap')[0]
-
+arp.show()
 # rc4 seed est composé de IV+clé
 seed = arp.iv+key
 # Calcul du nouvel ICV en effectuant un CRC du message
-icv = binascii.crc32(message)
-icv_clear='{:x}'.format(icv).decode("hex")
+icv = binascii.crc32(message) & 0xffffffff
+a = struct.pack('I', icv)
+ #01001010 10101100 11111011 10101111
+#a = hex_bytes(0x7FFFFFFF)
+
+
+#print(bitstring_to_bytes(icv))
+#our_bytes = icv.to_bytes(4, byteorder='big', signed=True)
+
+#icv = "0x4AACFBAF"
+#icv_bytes = bytes(icv)
+#icv_encrypted='{:x}'.format(icv).encode("bytes")
+#icv_clear='{:x}'.format(icv).decode("hex")
 
 #texte en clair (message + ICV)
-message_clear=message + icv_clear
+message_clear=message + a
 
+
+#cryptedMessage = rc4.rc4crypt(message, seed)
+#cryptedICV = rc4.rc4crypt(icv, seed)
 #Calcul du keystream
 cryptedText = rc4.rc4crypt(message_clear, seed)  
 
@@ -42,10 +68,17 @@ text_crypte=cryptedText[:-4]
 
 #remplacement du wepData par le message crypté
 arp.wepdata = text_crypte
+#arp.wepdata = cryptedMessage
 
 #remplacement de l'icv par l'icv crypté
+#icv_clear=struct.unpack("!L", icv_crypted)
+#arp.icv = long(icv_numerique)
+#icv_clear='{:x}'.format(icv_crypted).decode("hex")
+#arp.icv = icv_numerique
 arp.icv = icv_numerique
 
-arp.show()
+print 'Text: ' + arp.wepdata.encode("hex")
+print 'icv:  ' + icv_crypted.encode("hex")
+#print 'icv(num): ' + str(a)
 
 wrpcap("arp1.cap", arp)
