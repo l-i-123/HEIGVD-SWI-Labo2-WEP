906f434e7380e52e2fd851b0f7c5c5fb18a27f44
Elie <elie.kin63@gmail.com>
2019-04-06 15:40:03 +0200

    manual-encryption commenté

--- files/manual-encryption.py
+++ files/manual-encryption.py
@@ -25,60 +25,46 @@ def bitstring_to_bytes(s):
     return bytes(b[::-1])
 '''
 
-#Cle wep AA:AA:AA:AA:AA
-key='\xaa\xaa\xaa\xaa\xab'
-message = "surprisemotherfuckersurprisemotherfu"
-#messageBin = binascii.a2b_base64(message)
+# Cle wep AA:AA:AA:AA:AA
+key='\xaa\xaa\xaa\xaa\xaa'
 
-#lecture de message chiffré - rdpcap retourne toujours un array, même si la capture contient un seul paquet
+# Message à crypter
+message = "super!!!super!!!super!!!super!!!supe"
+
+# Récupération du fichier .cap fourni - rdpcap retourne toujours un array, même si la capture contient un seul paquet
 arp = rdpcap('arp.cap')[0]
-arp.show()
+
 # rc4 seed est composé de IV+clé
 seed = arp.iv+key
+
 # Calcul du nouvel ICV en effectuant un CRC du message
+# l'instruction & 0xffffffff permet de toujours retourner un icv positif
 icv = binascii.crc32(message) & 0xffffffff
-a = struct.pack('I', icv)
- #01001010 10101100 11111011 10101111
-#a = hex_bytes(0x7FFFFFFF)
+# Conversion de l'ICV au format int
+icv_int = struct.pack('I', icv)
 
+# Concaténation du message et de l'ICV
+message_clear = message + icv_int
 
-#print(bitstring_to_bytes(icv))
-#our_bytes = icv.to_bytes(4, byteorder='big', signed=True)
-
-#icv = "0x4AACFBAF"
-#icv_bytes = bytes(icv)
-#icv_encrypted='{:x}'.format(icv).encode("bytes")
-#icv_clear='{:x}'.format(icv).decode("hex")
-
-#texte en clair (message + ICV)
-message_clear=message + a
-
-
-#cryptedMessage = rc4.rc4crypt(message, seed)
-#cryptedICV = rc4.rc4crypt(icv, seed)
-#Calcul du keystream
+# Calcul du frame body en faisant keystream xor message_clear
 cryptedText = rc4.rc4crypt(message_clear, seed)  
 
-#récupération del'ICV crypté
+# Récupération de l'ICV crypté
 icv_crypted=cryptedText[-4:]
 (icv_numerique,)=struct.unpack('!L', icv_crypted)
 
-#calcul du frame body en faisant keystream xor (data + ICV)
+# Récupération du message crypté
 text_crypte=cryptedText[:-4] 
 
-#remplacement du wepData par le message crypté
+# Remplacement du wepData par le message crypté
 arp.wepdata = text_crypte
-#arp.wepdata = cryptedMessage
 
-#remplacement de l'icv par l'icv crypté
-#icv_clear=struct.unpack("!L", icv_crypted)
-#arp.icv = long(icv_numerique)
-#icv_clear='{:x}'.format(icv_crypted).decode("hex")
-#arp.icv = icv_numerique
+# Remplacement de l'icv par l'icv crypté
 arp.icv = icv_numerique
 
+# Affichage de quelques information
 print 'Text: ' + arp.wepdata.encode("hex")
 print 'icv:  ' + icv_crypted.encode("hex")
-#print 'icv(num): ' + str(a)
 
+# Ecriture de la nouvelle trame dans le fichier arp1.cap
 wrpcap("arp1.cap", arp)
